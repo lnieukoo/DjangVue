66d555e0964b46b55f00057cb0b56d794c68d488
Leroy Nieukoop <leroy.nieukoop@gmail.com>
2020-07-27 16:17:29 +0800

    added vuex store.js

--- webapp/static/js/app.js
+++ webapp/static/js/app.js
@@ -1,8 +1,10 @@
 import MainContent from './components/MainContent.js'
+import store from './store.js'
 
 const app = new Vue({
     el: "#app",
     delimiters: ["[[", "]]"],
     data: {
     },
+    store
   });
---
+++ webapp/static/js/store.js
@@ -0,0 +1,11 @@
+const store = new Vuex.Store({
+    state: {
+      navbar: 'home'
+    },
+    mutations: {
+      },
+}) 
+
+
+
+export default store
